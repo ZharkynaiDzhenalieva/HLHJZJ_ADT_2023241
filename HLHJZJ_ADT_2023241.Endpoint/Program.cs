d6a8e1948011f1a1d431d6eefca341c4c286fc02
Жаркынай Женалиева <jaka@Zarkynajs-MacBook-Pro.local>
2023-10-21 21:32:41 +0200

    structure

---
+++ HLHJZJ_ADT_2023241.Endpoint/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace HLHJZJ_ADT_2023241.Endpoint;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/", () => "Hello World!");
+
+        app.Run();
+    }
+}
+
