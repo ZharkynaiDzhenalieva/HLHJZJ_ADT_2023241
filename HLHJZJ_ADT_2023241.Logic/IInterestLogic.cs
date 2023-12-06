b193fd68de3125e58c5a9b86e783ce60dfd849ef
jaka <jenalievas2003@stud.uni-obuda.hu>
2023-12-06 21:26:59 +0100

    Logic updated

---
+++ HLHJZJ_ADT_2023241.Logic/IInterestLogic.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using HLHJZJ_ADT_2023241.Models;
+
+namespace HLHJZJ_ADT_2023241.Logic
+{
+	public interface IInterestLogic
+	{
+        void Create(Interest obj);
+        Interest Read(int id);
+        IQueryable<Interest> ReadAll();
+        void Update(Interest obj);
+        void Delete(int id);
+    }
+}
+
---
+++ HLHJZJ_ADT_2023241.Logic/InterestLogic.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using HLHJZJ_ADT_2023241.Models;
+using HLHJZJ_ADT_2023241.Repository;
+
+namespace HLHJZJ_ADT_2023241.Logic
+{
+	public class InterestLogic : IInterestLogic
+    { 
+    IRepository<Interest> interestRepo;
+
+    public InterestLogic(IRepository<Interest> interestRepo)
+    {
+        this.interestRepo = interestRepo;
+    }
+
+    public void Create(Interest obj)
+    {
+        interestRepo.Create(obj);
+    }
+
+    public void Delete(int id)
+    {
+            interestRepo.Delete(id);
+    }
+
+    public Interest Read(int id)
+    {
+        return interestRepo.Read(id);
+    }
+
+    public IQueryable<Interest> ReadAll()
+    {
+        return interestRepo.ReadAll();
+    }
+
+    public void Update(Interest obj)
+    {
+            interestRepo.Update(obj);
+    }
+
+    }
+}
+
